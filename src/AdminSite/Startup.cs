20776a2d025b0a65b26047ac35554c96a3428576
Neelavarsha Duddanahalli Kumar Mahesh <neelavarshad@microsoft.com>
2024-08-21 12:54:19 -0700

    Added configs to appsettings

--- src/AdminSite/Startup.cs
+++ src/AdminSite/Startup.cs
@@ -87,7 +87,7 @@ public class Startup
         {
             KnownUsers = this.Configuration["KnownUsers"],
         };
-        var clientCertificate = new X509Certificate2( config.ClientCertificate, config.ClientCertificatePassword);
+        var clientCertificate = new X509Certificate2(config.ClientCertificate, config.ClientCertificatePassword);
         var creds = new ClientCertificateCredential(config.TenantId.ToString(), config.ClientId.ToString(), clientCertificate);
 
 
